b492b2c12e31f4d8ef745a31dad5c3209f661bc5
BhtBristol <157963686+BhtBristol@users.noreply.github.com>
2024-04-28 17:28:17 +0100

    Update tapes_clean.py

--- tapes_clean.py
+++ tapes_clean.py
@@ -17,33 +17,33 @@ def read_csv_for_time_segments(file_path):
         reader = csv.reader(csvfile)
         rows = list(reader)
 
-    # 找到整个数据集中的最小时间戳
+    # Find the smallest timestamp in the entire dataset
     start_time = float('inf')
     for row in rows:
         time = float(row[0])
         start_time = min(start_time, time)
 
-    # 初始化60个时段的字典，每个时段的开盘和闭市数据初始为None
+    # Initialise a dictionary of 60 time slots, with the opening and closing data for each slot initially being None
     segments = {f'segment_{i+1}': {'open': None, 'close': None, 'max': None, 'min': None} for i in range(60)}
 
     for row in rows:
         time = float(row[0])
-        # 计算当前时间属于哪个时段（1-60）
+        # Calculate which time period the current time belongs to (1-60)
         segment_index = int((time - start_time) // 500) + 1
 
-        # 确保时段编号在1到60之间
+        # Ensure that time slots are numbered between 1 and 60
         if 1 <= segment_index <= 60:
             segment_key = f'segment_{segment_index}'
             if not segments[segment_key]['open']:
                 segments[segment_key]['open'] = row
             segments[segment_key]['close'] = row
-            # 更新最大值和最小值
+            # Update maximum and minimum values
             if segments[segment_key]['max'] is None or float(row[1]) > segments[segment_key]['max']:
                 segments[segment_key]['max'] = float(row[1])
             if segments[segment_key]['min'] is None or float(row[1]) < segments[segment_key]['min']:
                 segments[segment_key]['min'] = float(row[1])
 
-     # 打印 segments 字典的内容
+     # Print the contents of the segments dictionary
     for key, value in segments.items():
         print(key, value)
 
@@ -75,15 +75,13 @@ def process_multiple_csv(input_folder, output_file):
                     writer.writerow([
                         date,
                         segment_name,
-                        open_price[1],  # 开盘价（第二列）
-                        close_price[1],  # 闭市价（第二列）
-                        max_price,  # 最大值（第二列）
-                        min_price  # 最小值（第二列）
+                        open_price[1],  
+                        close_price[1],  
+                        max_price,  
+                        min_price  
                     ])
 
 
-
-# 使用示例
-input_folder = 'E:/mini/tapes'  # 更新为你的输入文件夹路径
-output_file = 'E:/mini/output2.csv'  # 更新为你的输出文件路径
+input_folder = 'E:/mini/tapes' 
+output_file = 'E:/mini/output2.csv'  
 process_multiple_csv(input_folder, output_file)
